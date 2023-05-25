c0fab43560aa5e064f097bf49b45b2efdef94631
DakilaLexter <lexterdumas@gmail.com>
2023-05-24 23:47:34 -0700

    blade short

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>event page</title>
-</head>
-<body>
-   <header> 
-        <nav>
-            <a href=''>Landing Page</a>
-            <a href=''>Home</a>
-            <a href=''>Profile</a>
-            <a href=''>Events</a>
-            <a href=''>News</a>
-            <a href=''>Contact</a>
-        </nav>
-   </header>
-    <h1>WELCOME TO EVENT PAGE</h1>
-</body>
-</html>
+@extends('layout.app')
+
+@section('content')
+     <h1>WELCOME TO EVENT PAGE</h1>
+
+@endsection
+   
+@section('title')
+EVENTS PAGE
+@endsection
+   
+   
