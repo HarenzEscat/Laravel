bcf77cba3a4d610e1bf559b014f95d4addd4991f
DakilaLexter <lexterdumas@gmail.com>
2023-06-07 22:51:49 -0700

    controllers

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
      <h1>WELCOME TO CONTACT US PAGE</h1>
+     <form action="{{route('contact.submit')}}" method="post">
+         @csrf
+         <div>
+             <label for="name">Name</label>
+             <input type="text" name="name" id="name">
+         </div>
+ 
+         <div>
+             <label for="email">Email Address</label>
+             <input type="email" name="email" id="email">
+         </div>
+ 
+ 
+         <div>
+             <label for="message">Message</label>
+             <input type="text" name="message" id="message">
+         </div>
+ 
+         <div>
+             <button>Submit</button>
+         </div>
+     </form>
 @endsection
    
 @section('title')
